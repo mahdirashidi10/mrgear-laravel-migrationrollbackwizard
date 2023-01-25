29f7e9d48ad09ad319bb180e74d74d433dc3d8b8
mahdirashidi <mahdirashidi.developer@gmail.com>
2023-01-25 23:06:41 +0330

    added config file, service provider

---
+++ src/ServiceProviders/MigrationRollbackWizardServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Danny\Migrations\ServiceProviders;
+
+use Danny\Migrations\Console\Commands\MigrationRollbackWizard;
+use Illuminate\Support\ServiceProvider;
+
+class MigrationRollbackWizardServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../Config/mrw.php' => config_path('mrw.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../Config/mrw.php', 'mrw');
+
+        $this->app->bind('migrate:rollback:wizard', MigrationRollbackWizard::class);
+
+
+        $this->commands([
+            'migrate:rollback:wizard',
+        ]);
+    }
+}
