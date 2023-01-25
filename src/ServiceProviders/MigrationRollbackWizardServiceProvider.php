2d774aaa900ab5b531a3a8c194e7aafbd977d2cb
mahdirashidi <mahdirashidi.developer@gmail.com>
2023-01-25 23:54:48 +0330

    automatic adding to providers list

--- src/ServiceProviders/MigrationRollbackWizardServiceProvider.php
+++ src/ServiceProviders/MigrationRollbackWizardServiceProvider.php
@@ -12,15 +12,17 @@ class MigrationRollbackWizardServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../Config/mrw.php' => config_path('mrw.php'),
         ], 'config');
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                MigrationRollbackWizard::class,
-            ]);
-        }
+
     }
 
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../Config/mrw.php', 'mrw');
+        $this->app->bind('migrate:rollback:wizard', MigrationRollbackWizard::class);
+
+
+        $this->commands([
+            'migrate:rollback:wizard',
+        ]);
     }
 }
