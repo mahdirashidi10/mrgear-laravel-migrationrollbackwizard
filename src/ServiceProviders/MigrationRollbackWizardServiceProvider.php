94c974a56de0f1d32e38fa471d7dcbb71e6c36bd
mahdirashidi <mahdirashidi.developer@gmail.com>
2023-01-25 23:17:43 +0330

    added config file, service provider

--- src/ServiceProviders/MigrationRollbackWizardServiceProvider.php
+++ src/ServiceProviders/MigrationRollbackWizardServiceProvider.php
@@ -12,17 +12,15 @@ class MigrationRollbackWizardServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../Config/mrw.php' => config_path('mrw.php'),
         ], 'config');
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MigrationRollbackWizard::class,
+            ]);
+        }
     }
 
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../Config/mrw.php', 'mrw');
-
-        $this->app->bind('migrate:rollback', MigrationRollbackWizard::class);
-
-
-        $this->commands([
-            'migrate:rollback:wizard',
-        ]);
     }
 }
